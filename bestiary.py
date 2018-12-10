952dc030610d0fd264af85b709b85b1e6da93b7f
Kai <34060097+Kaiz0r@users.noreply.github.com>
2018-12-10 19:12:08 +0000

    Add files via upload

--- bestiary.py
+++ bestiary.py
@@ -5,6 +5,7 @@ from functools import partial
 import json
 import dataclasses
 import os
+import contextlib
 
 scriptdir = os.path.dirname(os.path.abspath(__file__))+"/"
 bgui = None
@@ -35,18 +36,18 @@ def getConfig(parent="", key="", *, filename="config"):
 def setConfig(parent:str, key:str, value, *, filename="config", logging=False):
 	if logging:
 		AddLog(f"{parent} : {key} : {value}")
-	data = getConfig()
+	data = getConfig(filename=filename)
 	try:
 		data[parent][key] = value
 	except:
 		data[parent] = {}
 		data[parent][key] = value
 	with open(scriptdir+filename+'.json', "w") as s:
-		json.dump(data, s, indent=4, sort_keys=True)
+		json.dump(data, s, indent=4)
 
 def dumpConfig(data, filename="config"):
 	with open(scriptdir+filename+'.json', "w") as s:
-		json.dump(data, s, indent=4, sort_keys=True)
+		json.dump(data, s, indent=4)
 
 def touchConfig(parent:str, key:str, default, *, filename="config"):
 	with open(scriptdir+filename+".json") as f:
@@ -54,7 +55,7 @@ def touchConfig(parent:str, key:str, default, *, filename="config"):
 		try:
 			return data[parent][key]
 		except:
-			setConfig(parent, key, default)
+			setConfig(parent, key, default, filename=filename)
 			return default
 
 def load_game(game):
@@ -80,6 +81,7 @@ def load_game_from_list(game_list, sw):
 	
 	game = game_list.get(index)
 	print(game)
+	bgui.modules.delete(0, 'end')
 	try:
 		data = getConfig(filename=game)
 		print(data)
@@ -91,11 +93,16 @@ def load_game_from_list(game_list, sw):
 			bgui.fbut.config(state="normal")
 		else:
 			bgui.fbut.config(state="disable")
-		messagebox.showinfo(message=f"Author: {data['_META']['author']}\nGame: {data['_META']['game']}")	
+			
+		s = ""
+		for item in data['_META']:
+			if data['_META'][item] != "":
+				s += f"{item.capitalize()}: {data['_META'][item]}\n"
+		messagebox.showinfo(message=s)	
 		
 		for item in data:
 			if not item.startswith("_"): #Meta data keys
-				bgui.modules.insert(0, item)
+				bgui.modules.insert('end', item)
 		bgui.cur_game = game
 		sw.destroy()
 		
@@ -104,24 +111,28 @@ def load_game_from_list(game_list, sw):
 
 def add_mob(mob=False):
 	print(mob)
+	data = {}
 	mobdata = {}
+	w = tkinter.Tk()
 	if mob:
 		data = getConfig(filename=bgui.cur_game)
 		mobdata = data[bgui.currently_selected]
-		
-	w = tkinter.Tk()
-	w.title("Mob Create")
+		w.title(f"Mob Edit")
+	else:
+		w.title(f"Mob Create")
+
 	entries_names = []
 	entries_value = []
-	name_label = tkinter.Label(w, text="Name", width=8)
+	name_label = tkinter.Label(w, text="Name", width=12)
 	name_label.grid(column=0, row=0)
 	name_entry = tkinter.Entry(w)
 	name_entry.grid(column=1, row=0)
+	
 	if mob:
 		name_entry.insert(0, bgui.currently_selected)
 	
 	for key in mobdata:
-		le = tkinter.Entry(w)
+		le = tkinter.Entry(w, width=12)
 		le.grid(column=0, row=len(entries_names)+1)
 		le.insert(0, key)
 		lv = tkinter.Entry(w)
@@ -139,8 +150,7 @@ def add_mob(mob=False):
 	sv = partial(save_editor_mob, name_entry, entries_names, entries_value)
 	save_edit = tkinter.Button(w, text="SAVE", command=sv)
 	save_edit.grid(column=4, row=0)
-	
-	
+		
 def delete_mob():
 	if not tkinter.messagebox.askokcancel(message=f"Delete {bgui.currently_selected} from {bgui.cur_game}?"):
 		return
@@ -165,7 +175,7 @@ def save_editor_mob(name, en, ev):
 	reload_mobs()
 	
 def add_editor_line(en, ev, window):
-	le = tkinter.Entry(window, width=8)
+	le = tkinter.Entry(window, width=12)
 	le.grid(column=0, row=len(en)+1)
 	lv = tkinter.Entry(window)
 	lv.grid(column=1, row=len(ev)+1)
@@ -182,10 +192,11 @@ def load_mob():
 	data = getConfig(filename=bgui.cur_game)
 	print(data[bgui.currently_selected])
 	w = tkinter.Tk()
-	
-	mobs = tkinter.Listbox(w, height=15)
-	mobs.grid(rowspan=6, columnspan=4)
-	mobsc = tkinter.Scrollbar(w)
+	frame = tkinter.Frame(w)
+	frame.grid()
+	mobs = tkinter.Listbox(frame, height=20, width=40)
+	mobs.grid(rowspan=6, columnspan=4, sticky="nsew")
+	mobsc = tkinter.Scrollbar(frame)
 	mobsc.grid(column=4, row=0, rowspan=6, sticky="ns")
 	mobsc.config(command=mobs.yview)
 	mobs.config(yscrollcommand=mobsc.set)	
@@ -193,7 +204,7 @@ def load_mob():
 	mob = data[bgui.currently_selected]
 	w.title(bgui.currently_selected)
 	for item in mob:	
-		mobs.insert(1, f"{item}: {mob[item]}")
+		mobs.insert('end', f"{item}: {mob[item]}")
 	mobs.insert(0, bgui.currently_selected)	
 
 def reload_mobs():
@@ -210,7 +221,7 @@ def reload_mobs():
 		bgui.modules.delete(0, 'end')	
 		for item in data:
 			if item != "_META":
-				bgui.modules.insert(0, item)
+				bgui.modules.insert('end', item)
 		bgui.cur_game = game
 		
 	except FileNotFoundError:
@@ -229,6 +240,81 @@ def onselect(evt):
 def about():
 	print("None")
 
+def edit_game(new_game=False):
+	w = tkinter.Tk()
+	w.title("Game Editor")
+	print(bgui.cur_game)
+	if not new_game:
+		if bgui.cur_game == "":
+			messagebox.showerror(message="No game open.")
+			w.destroy()
+			return
+		w.title(f"Game Editor ({bgui.cur_game})")
+		data = getConfig(filename=bgui.cur_game)
+		
+	name_label = tkinter.Label(w, text="Name:")
+	name_label.grid(row=0, column=0)
+	name_entry = tkinter.Entry(w)
+	name_entry.grid(row=0, column=1)
+	author_label = tkinter.Label(w, text="Author: ")
+	author_label.grid(row=1, column=0)
+	author_entry = tkinter.Entry(w)
+	author_entry.grid(row=1, column=1)
+	contact_label = tkinter.Label(w, text="Contact: ")
+	contact_label.grid(row=2, column=0)
+	contact_entry = tkinter.Entry(w)
+	contact_entry.grid(row=2, column=1)
+	platforms_label = tkinter.Label(w, text="Platforms: ")
+	platforms_label.grid(row=3, column=0)
+	platforms_entry = tkinter.Entry(w)
+	platforms_entry.grid(row=3, column=1)
+	dev_label = tkinter.Label(w, text="Developer: ")
+	dev_label.grid(row=4, column=0)
+	dev_entry = tkinter.Entry(w)
+	dev_entry.grid(row=4, column=1)
+	notes_label = tkinter.Label(w, text="Notes: ")
+	notes_label.grid(row=5, column=0)
+	notes_entry = tkinter.Entry(w)
+	notes_entry.grid(row=5, column=1)
+	
+	filename_entry = tkinter.Entry(w)
+	filename_entry.grid(row=0, column=3)
+	if not new_game:
+		with contextlib.suppress(KeyError):
+			filename_entry.insert(0, bgui.cur_game)
+			name_entry.insert(0, data['_META']['game'])
+			author_entry.insert(0, data['_META']['author'])
+			contact_entry.insert(0, data['_META']['contact'])
+			platforms_entry.insert(0, data['_META']['platform'])
+			dev_entry.insert(0, data['_META']['developers'])
+			notes_entry.insert(0, data['_META']['notes'])
+		
+	save_game = partial(save_game_meta, filename_entry, name_entry, author_entry, contact_entry, platforms_entry, notes_entry, dev_entry, new_game)
+	save_button = tkinter.Button(w, text="SAVE ->", command=save_game).grid(row=0, column=2)
+
+def save_game_meta(filename, name, author, contact, platform, notes, devs, new_game):
+	if filename.get() == "":
+		messagebox.showerror(message="Filename is empty.")
+	data = {}
+	data['game'] = name.get()
+	data['author'] = author.get()
+	data['contact'] = contact.get()
+	data['platform'] = platform.get()
+	data['devs'] = devs.get()
+	data['notes'] = notes.get()
+	
+	if not new_game:
+		g = getConfig(filename=bgui.cur_game)
+		g['_META'] = data
+		dumpConfig(g, filename=bgui.cur_game)
+	else:
+		open(scriptdir+filename.get()+".json", 'w+').close()
+		with open(scriptdir+filename.get()+".json", 'w+') as f:
+			f.write("{}")
+		g = getConfig(filename=filename.get())
+		g['_META'] = data
+		dumpConfig(g, filename=filename.get())	
+		
 def find_game():
 	w = tkinter.Tk()
 	ls = tkinter.Listbox(w)
@@ -274,7 +360,10 @@ class bestiary:
 		self.filemenu = tkinter.Menu(self.mainwin)
 		self.menu.add_cascade(label="Bestiary", menu=self.filemenu)
 		self.filemenu.add_command(label="About", command=about)
-		self.filemenu.add_command(label="Find Game", command=find_game)
+		self.filemenu.add_command(label="Load Game", command=find_game)
+		self.filemenu.add_command(label="Edit Game", command=edit_game)
+		edit_game_load = partial(edit_game, True)
+		self.filemenu.add_command(label="New Game", command=edit_game_load)
 		self.filemenu.add_separator()
 		self.filemenu.add_command(label="Exit", command=self.mainwin.destroy)
 		#load_game("test")
